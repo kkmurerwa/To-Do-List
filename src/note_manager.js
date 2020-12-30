071c91ca7f6ab043de3ae43bbc56a250ab6968c0
xwaxes01 <xwaxes@gmail.com>
2020-12-30 14:36:49 +0300

    Added dynamic content

--- src/note_manager.js
+++ src/note_manager.js
@@ -1,10 +1,11 @@
 let myTodos = []
 
-function ToDo(title, description, dueDate, priority) {
+function ToDo(title, description, dueDate, priority, ...checkList) {
     this.Title = title
     this.Description = description
     this.DueDate = dueDate
     this.Priority = priority
+    this.CheckList = [...checkList]
 }
 
 function Note(title, description){
@@ -25,9 +26,9 @@ export function addTodo (title, description, date, priority){
 }
 
 export function createTestTodos(){
-    let todo1 = new ToDo("Title 1", "Description 1", "12/12/2020", "high")
-    let todo2 = new ToDo("Title 2", "Description 2", "13/12/2020", "low")
-    let todo3 = new ToDo("Title 3", "Description 3", "12/12/2020", "medium")
+    let todo1 = new ToDo("Title 1", "Description 1", "12/12/2020", "high", "dishes", "others")
+    let todo2 = new ToDo("Title 2", "Description 2", "13/12/2020", "low", "check 1", "check 2")
+    let todo3 = new ToDo("Title 3", "Description 3", "12/12/2020", "medium", "check 1", "check 2")
 
     myTodos.push(todo1, todo2, todo3)
 }
--- src/ui_manager.js
+++ src/ui_manager.js
@@ -15,7 +15,7 @@ export function createNoteListWindow(){
 
 function addTodoClicked(){
     // Add new todo to list and get instance of the new todo
-    let todo = addTodo("New todo", "Completely new todo", "12/12/1997", "high")
+    let todo = addTodo("New todo", "Completely new todo", "12/12/1997", "high", "some stuff", "other stuff")
 
     const todoItem = document.createElement("a")
     todoItem.innerHTML = todo.Title
@@ -44,13 +44,132 @@ export function displayTodos(){
     })
 }
 
+function bootstrapRow(){
+    const row = document.createElement("div")
+    row.classList.add("row")
+
+    return row
+}
+
+function bootstrapColumn(colLength){
+    const column = document.createElement("div")
+    column.classList.add(`col-md-${colLength}`)
+
+    return column
+}
+
 function displayTodoDetails(todoItem){
-    const todoTitle = document.getElementById("todo-item-title")
-    todoTitle.innerHTML = todoItem.Title
+    const noteInfoColumn = document.getElementById("note-info-column")
+    noteInfoColumn.innerHTML = ""
 
-    const todoDate = document.getElementById("todo-item-date")
-    todoDate.innerHTML = todoItem.DueDate
+    /* Create first row and append items */
+    const rowTitle = bootstrapRow()
 
-    const todoDesc = document.getElementById("todo-item-description")
-    todoDesc.innerHTML = todoItem.Description
+    const columnTitle = bootstrapColumn(9)
+
+    const columnDate = bootstrapColumn(3)
+
+    const todoTitle = document.createElement("input")
+    todoTitle.classList.add("form-control", "text-center", "todo-title")
+    todoTitle.setAttribute("type", "text")
+    todoTitle.value = todoItem.Title
+
+    const todoDate = document.createElement("input")
+    todoDate.classList.add("form-control", "todo-text")
+    todoDate.setAttribute("type", "date")
+    todoDate.value = todoItem.DueDate
+
+    columnTitle.append(todoTitle)
+    columnDate.append(todoDate)
+
+    rowTitle.append(columnTitle, columnDate)
+
+    /* Create description and append items */
+    const rowDescription = bootstrapRow()
+
+    const columnDescriptionTitle = bootstrapColumn(12)
+
+    const todoDescriptionTitle = document.createElement("h3")
+    todoDescriptionTitle.innerHTML = `<i class="fa fa-list"></i> Description`
+
+    const columnDescriptionContent = bootstrapColumn(12)
+
+    const columnDescText = document.createElement("input")
+    columnDescText.classList.add("form-control", "todo-text")
+    columnDescText.setAttribute("type", "text")
+    columnDescText.value = todoItem.Title
+
+    columnDescriptionContent.append(columnDescText)
+
+    columnDescriptionTitle.append(todoDescriptionTitle)
+    
+    rowDescription.append(columnDescriptionTitle, columnDescriptionContent)
+
+    /* Create and append checklist items */
+        // Title
+    const rowChecklist = bootstrapRow()
+
+    const columnChecklistTitle = bootstrapColumn(12)
+
+    const todoChecklistTitle = document.createElement("h3")
+    todoChecklistTitle.innerHTML = `<i class="fa fa-check-square"></i> Checklist`
+
+    columnChecklistTitle.append(todoChecklistTitle)
+
+        // content
+    const columnChecklistContent = bootstrapColumn(12)
+
+    const columnChecklistContentContainer = document.createElement("div")
+    columnChecklistContentContainer.classList.add("form-check")
+
+
+    todoItem.CheckList.forEach(elem => {
+        const checkListItemContainer = document.createElement("p")
+        checkListItemContainer.classList.add("check-list-container")
+
+
+        const checkListItemBox = document.createElement("input")
+        const checkListItemLabel = document.createElement("label")
+
+        checkListItemBox.setAttribute("class", "form-check-input")
+        checkListItemBox.setAttribute("type", "checkbox")
+        checkListItemBox.setAttribute("id", "checkBox")
+        checkListItemLabel.setAttribute("class", "form-check-label")
+        checkListItemLabel.setAttribute("for", "checkBox")
+        checkListItemLabel.innerHTML = elem
+
+        checkListItemContainer.append(checkListItemBox, checkListItemLabel)
+
+        columnChecklistContentContainer.append(checkListItemContainer)
+
+    })
+
+
+    columnChecklistContent.append(columnChecklistContentContainer)
+
+    rowChecklist.append(columnChecklistTitle, columnChecklistContent)
+
+
+    noteInfoColumn.append(rowTitle, rowDescription, rowChecklist, todoItemOptionButtonsContainer())    
+}
+
+function todoItemOptionButtonsContainer(){
+    const buttonRows = bootstrapRow()
+    buttonRows.classList.add("text-center")
+
+    const buttonSave = document.createElement("button")
+    buttonSave.classList.add("btn", "btn-success", "options-button")
+    buttonSave.innerHTML = "Save"
+
+    const buttonDelete = document.createElement("button")
+    buttonDelete.classList.add("btn", "btn-danger", "options-button")
+    buttonDelete.innerHTML = "Delete"
+
+    buttonRows.append(bootstrapColumn(1), buttonSave, bootstrapColumn(1), buttonDelete, bootstrapColumn(1))
+
+    return buttonRows
+}
+
+function displayNewTodoForm(){
+
 }
